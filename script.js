8b49d2a6c1d5f2789ce277b7d8ed398f40445f4b
MohamedAEmara <medoemara104@gmail.com>
2023-02-26 13:03:27 +0200

    Finish the webpage

--- script.js
+++ script.js
@@ -13,8 +13,25 @@ window.addEventListener('keydown', function(e){
 
 
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+
+    // apply this function but after 500 ms as a delay 
+    setTimeout(() => {
+        key.classList.remove('playing');
+    }, 500);
+    
+    // key.classList.remove('playing');
     console.log(key);
+    // const haha = document.getElementsByTagName(`audio["${e.keycode}"]`);
+    // this.alert(haha);
 });
 
+function removeClick(){
+    key.classList.remove('playing');
+}
 
+function huh(){
+    let x = 1;
+    return 1;
+}
 
